fbb0fcd80a0d31b2f96652a740d8c164333cc03d
Jose Betancourth <josemanuel0308ba@gmail.com>
2026-05-11 20:33:55 -0500

    fix otra vez, falto el password jaja

--- services/editoriales/conexion.py
+++ services/editoriales/conexion.py
@@ -8,7 +8,7 @@ mi_db = mysql.connector.connect(
     host="localhost",
     port=3306,
     user="root",
-    password="123456789",
+    password="root",
     database="biblioteca"
 )
 mi_cursor = mi_db.cursor()
